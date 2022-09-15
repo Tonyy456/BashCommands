546d25172ccaa8fef3b56b9d513fd4c322aebbaf
Anthony D'Alesandro <ajdalesandro0115@gmail.com>
2022-09-15 11:42:05 -0400

    fixed setup

--- setup.sh
+++ setup.sh
@@ -9,7 +9,13 @@ INSTALL_DIR=$(dirname $SCRIPT_DIR)
 touch $SCRIPT_DIR/scripts/save_files/saved_edits.txt
 touch $SCRIPT_DIR/scripts/save_files/saved_jumps.txt
 touch $SCRIPT_DIR/scripts/save_files/store_copy.txt
+touch $SCRIPT_DIR/files/pf
 
-echo alias bash=\'. $SCRIPT_DIR/files/bashconfig\' >> $SCRIPT_DIR/files/bashconfig
+#replace lines
+sed -i "s@.*alias.*@@g" $SCRIPT_DIR/files/pf
+sed -i "s@.*$SCRIPT_DIR/files/bashconfig@@g" ~/.bashrc
+sed -i '/^[[:blank:]]*$/ d' $SCRIPT_DIR/files/pf
+
+echo alias bash=\'. $SCRIPT_DIR/files/bashconfig\' >> $SCRIPT_DIR/files/pf
 echo . $SCRIPT_DIR/files/bashconfig >> ~/.bashrc
 source ~/.bashrc
